a500e1d8f6a0dfcd93a213dba3b6de7aed6efb2d
DungHoang <kathymaidung@gmail.com>
2021-07-22 20:26:28 +0300

    add Navigation in Main

--- components/MainComponent.tsx
+++ components/MainComponent.tsx
@@ -1,24 +1,22 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
 import Menu from './MenuComponent';
+import { NavigationContainer } from '@react-navigation/native';
+import { RootStackParamList } from '../shared/types';
+
+const MenuNavigator = createStackNavigator<RootStackParamList>();
 
 export default function MainComponent() {
-    const [dishes, setDishes] = useState(DISHES);
-    const [selectedDish, setSelectedDish] = useState<number | null>(null);
-
-    const dish = dishes.find((dish) => dish.id === selectedDish);
-
-    function onDishSelect(dishId: number) {
-        setSelectedDish(dishId)
-    }
-
     return (
-        <View>
-            <Menu dishes={dishes} onPress={(dishId: number) => onDishSelect(dishId)} />
-            {dish && <Dishdetail dish={dish} />}
-        </View>
+        <NavigationContainer>
+            <MenuNavigator.Navigator>
+                <MenuNavigator.Screen name="Menu" component={Menu} />
+                <MenuNavigator.Screen name="DishDetail" component={Dishdetail} />
+            </MenuNavigator.Navigator>
+        </NavigationContainer>
     )
 }
