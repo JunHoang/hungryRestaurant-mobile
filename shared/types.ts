3a8443ed62b37162d502774194d238e20640424a
DungHoang <kathymaidung@gmail.com>
2021-07-23 15:55:31 +0300

    add more data and type

--- shared/types.ts
+++ shared/types.ts
@@ -11,6 +11,35 @@ export type Dish = {
     description: string
 }
 
+export type Comment = {
+    id: number,
+    dishId: number,
+    rating: number,
+    comment: string,
+    author: string,
+    date: string
+}
+
+export type Leader = {
+    id: number,
+    name: string,
+    image: string,
+    designation: string,
+    abbr: string,
+    featured: boolean,
+    description: string
+}
+
+export type Promotion = {
+    id: number,
+    name: string,
+    image: string,
+    label: string,
+    price: string,
+    featured: boolean,
+    description: string
+}
+
 export type MenuStackParamList = {
     Menu: undefined;
     DishDetail: { dishId: number };
